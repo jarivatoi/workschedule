9135a1919905d5bcd59cb5c55e2ffb220ed9fe6e
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-07-16 03:35:33 +0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: '/anwh/',
+  base: '/workschedule/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
