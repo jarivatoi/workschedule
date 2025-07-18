d947a89a855b9ce4e130febdf91dd3ce5bbc0971
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-07-18 11:27:51 +0400

    Debug and fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.VITE_DEPLOY_TARGET === 'github' ? '/workschedule/' : '/',
+  base: process.env.VITE_DEPLOY_TARGET === 'netlify' ? '/' : '/workschedule/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
